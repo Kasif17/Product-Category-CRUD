de09c55d4bb6f6c9f2c431a064e45c4d807a6150
Kasif17 <developer786kasif@gmail.com>
2024-03-09 14:42:23 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const  ejs = require('ejs');
+
+const app = express()
+const port = 3000
+
+// database connection 
+const db = require("./dbconnection");
+
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+
+
+
+// configure middleware
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(path.join(__dirname, 'public', 'dashboard','css')));
+app.use(express.static(path.join(__dirname, 'public', 'dashboard','img')));
+app.use(express.static(path.join(__dirname, 'public', 'dashboard','lib')));
+app.use(express.static(publicDirectory));
+
+
+//------------------------------------ Routes Parts ----------------------------------//
+app.use("/category",categoryRoutes);
+app.use("/product",productRoutes);
+//------------------------------------ (HomeRoute) ----------------------------------//
+
+
+// app.get('/', (req, res) => {
+//   const sql = "SELECT * FROM product INNER JOIN category WHERE product.category = category.idc";
+//   db.query(sql, (err, rows) => {
+//     if (err) {
+//       console.error("Error executing SQL query:", err);
+//       return res.status(500).send("Internal Server Error");
+//     }
+//     console.log("Query results:", rows);
+//     res.render('index', {
+//       title: 'E-com Relation',
+//       rows: rows
+//     });
+//   });
+// });
+
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM product INNER JOIN category ON product.categoryId = category.idc";
+    db.query(sql, (err, rows) => {
+        console.log(rows);
+        if (err) throw err;
+        res.render('index', {
+            title: 'E-com Relation',
+            rows: rows
+        });
+    });
+});
+
+
+// app.get('/', (req, res) => {
+//   const sql = "SELECT * FROM product INNER JOIN category WHERE product.category = category.idc";
+//      db.query(sql, (err, rows) => {
+//       console.log(rows);
+//          if (err) throw err;
+//          res.render('index', {
+//           title : 'E-com Relation',
+//              rows: rows
+//          });
+ 
+//      })
+ 
+//  });
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
